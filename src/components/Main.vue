3177af5f6be5f23842e1fd215cc01f6ef0f78ab5
KaterinaProkopenko <ekaprokopenko97@gmail.com>
2022-04-01 13:03:37 +0200

    Inizializzazione

---
+++ src/components/Main.vue
@@ -0,0 +1,29 @@
+<template>
+  <main>
+      <MainCard/>
+  </main>
+</template>
+
+<script>
+import axios from "axios"
+import MainCard from "./MainCard.vue"
+
+export default {
+    name: 'indexMain',
+    components: {
+        MainCard,
+    },
+    created: function() {
+        axios.get('...').then((result) => {
+            console.log(result);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/MainCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="film-card">
+
+  </div>
+</template>
+
+<script>
+export default {
+    name:'MainCard'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
